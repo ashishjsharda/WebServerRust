5b69c676ce72eac2cee5afbe3fc6c2ebc338ca4e
Ashish Sharda <ashishjsharda@gmail.com>
2022-06-21 04:30:38 -0500

    Gen random number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+extern crate rand;
+
 use std::io;
 use rand::Rng;
 fn main() {
    println!("Random number generator");
+   let rand_num=rand::thread_rng().gen_range(1,101);
+   println!("Random num generated is {}",rand_num);
 
 }
