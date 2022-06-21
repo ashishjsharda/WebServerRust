506e179851f0a1c1be46671b06e1cb837b986f31
Ashish Sharda <ashishjsharda@gmail.com>
2022-06-21 04:27:12 -0500

    first commit

---
+++ target/debug/RustTutorial.d
@@ -0,0 +1,1 @@
+C:\Users\ashis\CLionProjects\RustTutorial\target\debug\RustTutorial.exe: C:\Users\ashis\CLionProjects\RustTutorial\src\main.rs
---
+++ target/debug/deps/RustTutorial.d
@@ -0,0 +1,5 @@
+C:\Users\ashis\CLionProjects\RustTutorial\target\debug\deps\RustTutorial.exe: src\main.rs
+
+C:\Users\ashis\CLionProjects\RustTutorial\target\debug\deps\RustTutorial.d: src\main.rs
+
+src\main.rs:
