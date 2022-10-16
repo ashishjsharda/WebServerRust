f6ca4a0360ea7ab107e1445ae7dfe736712a51a0
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-16 06:07:01 -0500

    implement kafka events buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
+use std::collections::HashMap;
+use rdkafka::ClientConfig;
+use rdkafka::producer::FutureProducer;
+use serde_json::Value;
+
 pub struct ProducerConfig{
     ack_timeout: u64,
     brokers: Vec<String>,
 }
+
+pub struct KafkaEventsBuffer {}
+impl KafkaEventsBuffer {
+    pub fn new() -> KafkaEventsBuffer {
+        KafkaEventsBuffer {}
+    }
+    pub fn send(&self, producer: &FutureProducer, topic: &str, key: &str, value: &str) {
+        let _ = producer.send_copy(topic, Some(key), Some(value), None, None);
+    }
