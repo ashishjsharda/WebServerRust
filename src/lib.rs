89b32543fac87ba3b6feb1f92155cd0a6d5bf2aa
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-16 06:03:11 -0500

    Add lib rs and import mod

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub struct ProducerConfig{
+    ack_timeout: u64,
+    brokers: Vec<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
+mod lib;
+
 use std::io;
 
 fn main() {
-    println!("Input guess: ");
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    println!("WIP")
 
 }
