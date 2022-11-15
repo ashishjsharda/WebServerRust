65e851dd48fd1931370e6be1a1e0c433bc25bc73
Ashish Sharda <ashishjsharda@gmail.com>
2022-11-15 06:55:32 -0600

    Add for in

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::net::TcpListener;
 fn main() {
     let _listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in _listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
 
 }
